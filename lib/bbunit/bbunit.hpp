e459c55081f6f8fa67a1f1e90a9f8868928e38db
Mark <mark@annoanno.dk>
2023-09-22 14:40:14 +0200

    Return TestResult from assertions

--- lib/bbunit/bbunit.hpp
+++ lib/bbunit/bbunit.hpp
@@ -31,6 +31,10 @@ void setColor(int colorCode)
 
 namespace BBUnit {
 
+    struct TestResult {
+        bool passed;
+    };
+
     class TestCase {
     public:
         TestCase* run()
@@ -53,75 +57,75 @@ namespace BBUnit {
     protected:
         virtual void test() = 0;
 
-        void assertEquals(int expected, int actual, const char *message)
+        TestResult assertEquals(int expected, int actual, const char *message)
         {
-            assert(expected == actual, message);
+            return assert(expected == actual, message);
         }
 
-        void assertEquals(double expected, double actual, const char *message)
+        TestResult assertEquals(double expected, double actual, const char *message)
         {
-            assert(expected == actual, message);
+            return assert(expected == actual, message);
         }
 
-        void assertEquals(bool expected, bool actual, const char *message)
+        TestResult assertEquals(bool expected, bool actual, const char *message)
         {
-            assert(expected == actual, message);
+            return assert(expected == actual, message);
         }
 
-        void assertEquals(const std::string& expected, const std::string& actual, const char *message)
+        TestResult assertEquals(const std::string& expected, const std::string& actual, const char *message)
         {
-            assert(expected == actual, message);
+            return assert(expected == actual, message);
         }
 
-        void assertGreaterThan(int compare, int actual, const char *message)
+        TestResult assertGreaterThan(int compare, int actual, const char *message)
         {
-            assert(actual > compare, message);
+            return assert(actual > compare, message);
         }
 
-        void assertGreaterThan(double compare, double actual, const char *message)
+        TestResult assertGreaterThan(double compare, double actual, const char *message)
         {
-            assert(actual > compare, message);
+            return assert(actual > compare, message);
         }
 
-        void assertTrue(bool actual, const char* message)
+        TestResult assertTrue(bool actual, const char* message)
         {
-            assert(actual, message);
+            return assert(actual, message);
         }
 
-        void assertFalse(bool actual, const char* message)
+        TestResult assertFalse(bool actual, const char* message)
         {
-            assert(!actual, message);
-        }
-
-        template <typename T>
-        void assertCount(unsigned int expected, std::vector<T> vector, const char* message)
-        {
-            assert(expected == vector.size(), message);
+            return assert(!actual, message);
         }
 
         template <typename T>
-        void assertEmpty(std::vector<T> vector, const char* message)
+        TestResult assertCount(unsigned int expected, std::vector<T> vector, const char* message)
         {
-            assert(vector.size() == 0, message);
+            return assert(expected == vector.size(), message);
+        }
+
+        template <typename T>
+        TestResult assertEmpty(std::vector<T> vector, const char* message)
+        {
+            return assert(vector.size() == 0, message);
         }
 
         template <typename K, typename V>
-        void assertCount(unsigned int expected, std::map<K, V> map, const char* message)
+        TestResult assertCount(unsigned int expected, std::map<K, V> map, const char* message)
         {
-            assert(expected == map.size(), message);
+            return assert(expected == map.size(), message);
         }
 
         template <typename K, typename V>
-        void assertEmpty(std::map<K, V> map, const char* message)
+        TestResult assertEmpty(std::map<K, V> map, const char* message)
         {
-            assert(map.size() == 0, message);
+            return assert(map.size() == 0, message);
         }
 
     private:
         unsigned int assertions = 0,
             passed = 0;
 
-        void assert(bool testPassed, const std::string& message)
+        TestResult assert(bool testPassed, const std::string& message)
         {
             assertions++;
 
@@ -138,6 +142,10 @@ namespace BBUnit {
 
             setColor(FOREGROUND_RESET | BACKGROUND_RESET);
             std::cout << " " << message << std::endl;
+
+            return {
+                .passed = testPassed,
+            };
         }
 
     };
